08798ee7d3b5022455306f15f37aab709f666383
ravi <ravi.kumar127@wipro.com>
2017-01-21 18:41:53 +0000

    learn redux

---
+++ app/app.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+//Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
+//app css
+require('style!css!sass!applicationStyles')
+
+ReactDOM.render(
+  <div><h1>React bolier plate 3</h1></div>,
+  document.getElementById('app')
+);
+
+require("./redux-example");
---
+++ app/redux-example.jsx
@@ -0,0 +1,8 @@
+var redux = require('redux');
+
+console.log("redux example");
+
+function add(a,b) {
+  return a+b;
+}
+add(2,3);
