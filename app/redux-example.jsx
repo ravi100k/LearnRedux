3c4b9f6a9d89a5cf8df9a7b3a88474c35e6a6c75
ravi <ravi.kumar127@wipro.com>
2017-01-27 07:42:41 +0000

    dispatch

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -8,9 +8,24 @@ console.log("redux example");
 // add(2,3);
 var reducer  = (state ={name: 'anonymous'}, action) => {
   //state = state || {name: 'anonymous'};
-  return state;
+  switch (action.type) {
+    case 'CHANGE_NAME':
+        return {
+          ...state,
+          name: action.name
+        };
+    default:
+    return state;
+  }
 };
 
 var store = redux.createStore(reducer);
 var currentState =  store.getState();
 console.log('currentState', currentState);
+
+store.dispatch({
+  type:'CHANGE_NAME',
+  name: 'Ravi'
+});
+
+console.log('name sholud be ravi', store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -8,8 +8,22 @@ var reducer  = (state, action) =>
     showCompleted: false,
      todos:[]
    };
-  return state;
+   switch (action.type) {
+     case 'CHANGE_TEXT':
+         return {
+           ...state,
+           searchText: action.searchText
+         };
+     default:
+     return state;
+   }
 };
 
 var store = redux.createStore(reducer);
+
+store.dispatch({
+  type:'CHANGE_TEXT',
+  searchText: 'Ravi'
+});
+
 console.log('currentState', store.getState());
